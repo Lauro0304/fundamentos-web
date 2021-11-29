61e9726406faacd1d2491c3757200602e4f98fb7
unknown <lauromatheus64@gmail.com>
2021-11-29 15:15:12 -0300

    @Lauro0304 [Task: validacao]

---
+++ bootstrap/js/formulario.js
@@ -0,0 +1,19 @@
+$('#validacao').submit(function(){
+var email = $('#email');
+
+if (email.val() ==''){
+    alert('Preencha o email!');
+
+    return false;
+}
+var assunto =$('#assunto');
+
+if (assunto.val() ==''){
+    alert('Preencha o Assunto!');
+
+    return false;
+}
+
+    return false;
+   
+});
